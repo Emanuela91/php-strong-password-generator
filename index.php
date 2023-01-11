2997b6f3209e2c9de8dbf8c39046c9fa5c06c930
Emanuela91 <emanuela.marson91@gmail.com>
2023-01-11 15:14:33 +0100

    aggiunto form lunghezza psw

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Generatort</title>
+</head>
+
+<body>
+    <!-- form per la lunghezza della password -->
+    <form method="get">
+        <label for="length">Lunghezza Password</label>
+        <input type="text" name="length">
+        <input type="submit" value="Invia">
+
+    </form>
+
+</body>
+
+</html>
