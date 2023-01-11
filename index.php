0aa6ded42616e3469788fafd990fcbb81836fd4e
Emanuela91 <emanuela.marson91@gmail.com>
2023-01-11 16:05:26 +0100

    aggiunta funzione per creare la password

--- index.php
+++ index.php
@@ -13,7 +13,21 @@
     <form method="get">
         <label for="length">Lunghezza Password</label>
         <input type="text" name="length">
-        <input type="submit" value="Invia">
+        <input type="submit" value="Genera Password">
+
+        <?php
+        // funzione per creare la password con numeri, lettere (min e maiusc), e carratteri speciali
+        function password_generator($length)
+        {
+            // vengono combinati più array con array_merge
+            $chars = array_merge(range(0, 5), range('a', 'q'), range('A', 'Q'), array('!', '&', '*', '?', '%'));
+            $password = "";
+            for ($i = 0; $i < $length; $i++) {
+                $password .= $chars[array_rand($chars)];
+            }
+        }
+
+        ?>
 
     </form>
 
